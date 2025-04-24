7cb46151e51d0f22a3ca74cc2999eed4503208e4
teebdo <tlave11@lsu.edu>
2025-04-24 07:56:06 -0500

    carosel

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ menuToggle.addEventListener('click', () => {
   menuToggle.setAttribute('aria-expanded',!expanded);
   menu.classList.toggle('expanded');
 });
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button.next');
+const prevButton = document.querySelector('.carousel-button.prev');
+
+let currentSlide = 0;
+
+function updateSlidePosition() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateSlidePosition();
+});
+
+prevButton.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateSlidePosition();
+});
+
+window.addEventListener('resize', updateSlidePosition);
